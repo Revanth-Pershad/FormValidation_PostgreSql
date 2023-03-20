ae97fb54a8a4768da24b6d640f7e990a8da923da
Revanth-Pershad <revanthpershad@gmail.com>
2023-03-20 16:33:02 +0530

    Added Id and viewing data through Id

--- app.js
+++ app.js
@@ -12,25 +12,51 @@ app.use(cors());
 const client = new Client({
   host: 'localhost',
   port: 5432,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database
+  user: 'postgres',
+  password: 'root',
+  database: 'postgres'
 });
 
 client.connect();
 
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  const query = 'SELECT * FROM users WHERE id = $1';
+  const values = [id];
+
+  console.log(`Data requested from ${req.hostname} for user ID ${id}`);
+
+  client.query(query, values, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({message: 'Error on the server fetching data'});
+    } else if (result.rows.length === 0) {
+      res.status(404).json({message: `No user found with ID ${id}`});
+    } else {
+      res.status(200).json(result.rows[0]);
+    }
+  });
+});
+
+
 app.post('/insert-data', (req, res) => {
-  const { first_Name, last_Name, email_, password_ } = req.body;
+  const { id, first_Name, last_Name, email_, password_ } = req.body;
 
-  const query = 'INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)';
-  const values = [first_Name, last_Name, email_, password_];
+  const query = 'INSERT INTO users (id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)';
+  const values = [id, first_Name, last_Name, email_, password_];
 
-  console.log(`Data sent from ${req.hostname}`)
-  console.log(values);
+  console.log(`Data sent from ${req.hostname}`);
 
   client.query(query, values, (err) => {
     if(err){
+      console.log(err);
+      if(err.code = '23505'){
+        console.log('Same key');
+        res.status(501).json({message : 'KEY already Exists'});
+      }
+      else{
       res.status(500).json({message : "Error on the server inserting data"})
+      }
     }
     else{
       res.status(200).json({message : "Insertion was a success"})
--- client/index.js
+++ client/index.js
@@ -7,47 +7,64 @@ const lastErr = document.getElementById('lastErr');
 const email = document.getElementById('email');
 const emailErr = document.getElementById('emailErr');
 
+const searchBtn = document.getElementById('Search');
+
 const form = document.getElementById('myForm');
 
-let isWorking = false;
+let firstWorking = false;
+let lastWorking = false;
+let emailWorking = false;
 
 firstName.addEventListener('input', (e) => {
-  if(e.target.value.length == 0){
+  userRegex = /^[a-zA-Z]+$/;
+
+  if (userRegex.test(e.target.value) == false) {
+    firstErr.innerText = "Use letters";
+    firstWorking = false;
+  }
+  else if (e.target.value.length == 0) {
     firstErr.innerText = ("Field is Empty");
-    isWorking = false;
+    firstWorking = false;
   }
-  else if(e.target.value.length < 3){
+  else if (e.target.value.length < 3) {
     firstErr.innerText = ("Text Length is very small");
-    isWorking = false;
+    firstWorking = false;
   }
-  else if(e.target.value.length > 10){
+  else if (e.target.value.length > 10) {
     firstErr.innerText = ("Text Length is very large");
-    isWorking = false;
+    firstWorking = false;
   }
-  else{
+  else {
     firstErr.innerText = ("");
-    isWorking = true;
+    firstWorking = true;
   }
 })
 
 
+
 lastName.addEventListener('input', (e) => {
-  if(e.target.value.length == 0){
-    lastErr.innerText = ("Field is Empty");
-    isWorking = false;
+  userRegex = /^[a-zA-Z]+$/;
+
+  if (userRegex.test(e.target.value) == false) {
+    lastErr.innerText = "Use letters";
+    lastWorking = false;
   }
-  else if(e.target.value.length < 3){
+  else if (e.target.value.length == 0) {
+    lastErr.innerText = ("Field is Empty");
+    lastWorking = false;
+  }
+  else if (e.target.value.length < 3) {
     console.log("Text is small");
     lastErr.innerText = ("Text Length is very small");
-    isWorking = false;
+    lastWorking = false;
   }
-  else if(e.target.value.length > 10){
+  else if (e.target.value.length > 10) {
     lastErr.innerText = ("Text Length is very large");
-    isWorking = false;
+    lastWorking = false;
   }
-  else{
+  else {
     lastErr.innerText = ("");
-    isWorking = true;
+    lastWorking = true;
   }
 })
 
@@ -57,41 +74,80 @@ email.addEventListener('input', (e) => {
 
   if (!emailRegex.test(email.value)) {
     emailErr.innerText = "Invalid Email";
-    isWorking = false;
-  }else{
+    emailWorking = false;
+  } else {
     emailErr.innerHTML = "";
-    isWorking = true;
+    emailWorking = true;
   }
 })
 
-form.addEventListener('submit', async(e) => {
-    e.preventDefault();
-  
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
   const first_Name = firstName.value;
   const last_Name = lastName.value;
   const email_ = email.value;
   const password_ = document.getElementById("password").value;
+  const id = document.getElementById("Id").value;
 
-  const data = { first_Name, last_Name, email_, password_ };
+  const data = {id, first_Name, last_Name, email_, password_ };
   console.log(data);
-  if(isWorking == false){
-    alert("Complete the form")
-  }else{
-    try {
-      const response = await fetch('http://localhost:3000/insert-data', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-      });
-  
-      const result = await response.json();
-    } catch (error) {
-      console.log(error.message);
+  if (!firstWorking || !lastWorking || !emailWorking) {
+    alert('Complete the errors');
+    return;
+  }
+  try {
+    const response = await fetch('http://localhost:3000/insert-data', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    if(response.status == 501){
+      alert('Key already Exists');
+      return;
     }
+  } catch (error) {
+    console.log(error.message);
+    return;
   }
   firstName.value = "";
   lastName.value = "";
   email.value = "";
   document.getElementById("password").value = "";
+  firstErr.innerText = "";
+  lastErr.innerText = "";
   alert('Form Submitted Succesfully!');
+
+})
+
+searchBtn.addEventListener('click', async(event) => {
+  event.preventDefault();
+  const id = document.getElementById("Id").value;
+  if(id == ''){
+    alert('Enter an Id');
+    return;
+  }
+  firstErr.innerText = "";
+  lastErr.innerText = "";
+  emailErr.innerText = "";
+  
+  try{
+    const response = await fetch(`http://localhost:3000/users/${id}`);
+    const result = await response.json();
+    console.log(result);
+    if(response.status == 200){
+      firstName.value = result.first_name;
+    lastName.value = result.last_name;
+    email.value = result.email;
+    }
+    else{
+      alert("Id not found");
+      return;
+    }
+  }
+  catch(err){
+    console.log(err);
+  }
 })
